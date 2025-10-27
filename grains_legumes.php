f3ca2cbcca4e3a393fa979e7f3098fbfeb1e9dcf
SamFems <samueloluwaferanmi37@gmail.com>
2025-10-27 18:40:54 +0100

    renaming

--- grains_legumes.php
+++ grains_legumes.php
@@ -7,16 +7,16 @@
     href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
     rel="stylesheet"
   />
-  <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css" />
-  <link rel="stylesheet" href="../assets/animate.min.css" />
-  <link rel="stylesheet" href="../assets/fontawesome/css/all.css" />
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+  <link rel="stylesheet" href="assets/animate.min.css" />
+  <link rel="stylesheet" href="assets/fontawesome/css/all.css" />
   <title>AgriLink - Farmers to Consumers</title>
   <style>
     body {
       background: linear-gradient(to left, #e8f5e9, #c8e6c9);
       font-family: "poppins", monospace;
     }
-    <?php require_once("../assets/style.php"); ?>
+    <?php require_once("assets/style.php"); ?>
   </style>
 </head>
 <body>
--- sign_farmer.php
+++ sign_farmer.php
@@ -7,17 +7,17 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="../assets/images/logo.png" />
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css" />
-    <link rel="stylesheet" href="../assets/animate.min.css" />
-    <link rel="stylesheet" href="../assets/fontawesome/css/all.css" />
+    <link rel="icon" href="assets/images/logo.png" />
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="assets/animate.min.css" />
+    <link rel="stylesheet" href="assets/fontawesome/css/all.css" />
     <title>AgriLink - Farmers to Consumers</title>
     <style>
       body {
         background: linear-gradient(to left, #e8f5e9, #c8e6c9);
         font-family: "poppins", monospace;
       }
-      <?php require_once("../assets/style.php"); ?>
+      <?php require_once("assets/style.php"); ?>
       .con {
         margin-top: 3%;
       }
